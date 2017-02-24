55a381472464c82e53f2bd70f32900b1e429062b
turboc1208 <chip.cox@coxdogs.com>
2017-02-23 19:35:02 -0600

    added support for dimming lights when people are away

--- homeaway.py
+++ homeaway.py
@@ -10,7 +10,7 @@
 #
 #################################
 import appdaemon.appapi as appapi
-import datetime
+from datetime import datetime, timedelta
 from utils import *
              
 class homeaway(appapi.AppDaemon):
@@ -27,11 +27,16 @@ class homeaway(appapi.AppDaemon):
                                     "switch.master_toilet_light":"off"}},
                 "sam":{"occupants":["device_tracker.scox0129_sc0129"],
                        "lights":{"light.sam_fan_switch":"off","light.sam_light_switch":"off",
-                                 "switch.sam_toilet_light":"off","switch.sam_toilet_fan":"off","switch.sam_vanity_switch":"off"}}}
+                                 "switch.sam_toilet_light":"off","switch.sam_toilet_fan":"off","switch.sam_vanity_switch":"off"}},
+                "wholehouse":{"occupants":["device_tracker.scox1209_scox1209","device_tracker.turboc1208_cc1208","device_tracker.scox0129_sc0129","device_tracker.ccox0605_ccox0605"],
+                              "lights":{"group.whole_house":"off"}},
+                "charlie":{"occupants":{"device_tracker.ccox0605_ccox0605"},
+                           "lights":{"light.charile_light_switch":"off","light.charlie_fan_switch":"50"}}}
     
+    self.tzoffset=-6
     # Setup Callback
     #self.run_minutely(self.timer_handler,datetime.datetime.now())          # for debugging
-    self.run_every(self.timer_handler,datetime.datetime.now(),15*60)
+    self.run_every(self.timer_handler,datetime.now()+timedelta(minutes=15),15*60)
 
     self.checkHomeState()  # Lets see what our condition is now rather than waiting 15 minutes.
 
@@ -40,9 +45,44 @@ class homeaway(appapi.AppDaemon):
     self.checkHomeState()
 
   # most of the work is done here
+  def _checklocationstate(self,location,occupants):
+    anyonehome=False
+    everyonehome=True
+    noonehome=True
+    occupants=self.rooms["wholehouse"]["occupants"]
+    for person in occupants:
+      personlocation=self.get_state(person)
+      #self.log("person {} is {}".format(person,personlocation))
+      if ((personlocation in location) if isinstance(location,list) else  (personlocation == location)):
+        anyonehome=True   
+        noonehome=False
+      else:
+        everyonehome=False
+    return (anyonehome,noonehome,everyonehome) 
+
+  def anyone_home(self):
+    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    return anyonehome
+  
+  def everyone_home(self):
+    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    return everyonehome
+
+  def noone_home(self):
+    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    return noonehome    
+  
   def checkHomeState(self):
+    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+
     self.log("everyone_home={} anyone_home={} noone_home={}".format(self.everyone_home(),self.anyone_home(),self.noone_home()))
     #self.log("rooms={}".format(self.rooms))
+
+    #self.log("{} state = {} vs {} ".format("light.den_fan_light",
+    #                                       datetime.strptime(self.get_state("light.den_fan_light","all")["last_updated"],
+    #                                                         "%Y-%m-%dT%H:%M:%S.%f+00:00")+timedelta(hours=self.tzoffset),
+    #                                       datetime.now()))
+
     if not self.everyone_home():             # if anyone is gone then lets see if we have a room to turn off lights in
       self.trackers = self.my_get_trackers() # get current status of all device_trackers
 
@@ -50,23 +90,53 @@ class homeaway(appapi.AppDaemon):
       for room in self.rooms:
         if not self.room_occupants_home(self.rooms[room]["occupants"]):  # if the room occupants are not home
           self.log("room {} occupants {} are not home".format(room,self.rooms[room]["occupants"]))
-          for light in self.rooms[room]["lights"]:                       # loop through the rooms lights
-            if self.rooms[room]["lights"][light]=="off":                 # if the light should be off
-              self.log("turning off {}".format(light))
-              self.turn_off(light)                                       # turn off the light
-            else:
-              self.log("turning on {}".format(light))                    # if the light should be on
-              self.turn_on(light)                                        # turn on the light
+          light_list=self.build_light_list(self.rooms[room]["lights"])
+          for light in light_list:                       # loop through the rooms lights
+            # what is the last time the lights state was changed
+            last_update=datetime.strptime(self.get_state(light,"all")["last_updated"],"%Y-%m-%dT%H:%M:%S.%f+00:00")+timedelta(hours=self.tzoffset)
+            if (last_update+timedelta(minutes=15)) < datetime.now():      # was the light state changed over (last update + 15 min)  ago
+              if light_list[light]=="off":                 # if the light should be off
+                if self.get_state(light)=="on":
+                  self.log("turning off {}".format(light))
+                  self.turn_off(light)                                       # turn off the light
+              elif light_list[light]=="on":
+                if self.get_state(light)=="off":
+                  self.log("turning on {}".format(light))                    # if the light should be on
+                  self.turn_on(light)                                        # turn on the light
+              else:
+                try:
+                  dim=int(light_list[light])
+                except ValueError:
+                  self.log("Transfering to unknown light state {}".format(light))
+                if self.get_state(light)=="on":
+                  self.log("adjusting power to {}".format(dim))
+                  self.turn_on(light,brightness=dim)
+            else:                                         # it has not been 15 minutes since the last time the light was manually turned on
+              self.log("light {} was last turned on at {} which is less than 15 minutes ago so leave it on".format(light,last_update))
         else:
           self.log("room {} occupants {} are home".format(room,self.rooms[room]["occupants"]))
     else:
       self.log("everyone is home")
+  
+  # build a list of lights to turn off doing it this way to account for a group being part of the lights
+  def build_light_list(self,lights):
+    out_list={}
+    for light in lights:
+      devtyp, devname = self.split_entity(light)
+      if devtyp=="group":
+        self.log("we have a group {}".format(light))
+        for group_light in self.get_state(light,attribute='all')["attributes"]["entity_id"]:
+          out_list[group_light]=lights[light]
+      else:
+        out_list[light]=lights[light]
+    return out_list
+
 
   # if any of the rooms occupants are home, return true, if no occupants are home, reutrn false
   def room_occupants_home(self,occupants):
     home=False
     for occupant in occupants:
-      if self.trackers[occupant]["state"]=="house":
+      if (self.trackers[occupant]["state"]=="house") or (self.trackers[occupant]["state"]=="home"):
         home=True
     return home
  
