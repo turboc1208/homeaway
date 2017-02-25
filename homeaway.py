fd2dd6c0ffa1f098d7706d5b8ef989f9ad32b864
turboc1208 <chip.cox@coxdogs.com>
2017-02-24 19:51:09 -0600

    handles people coming and going based on device_tracker state changes and turns on lights when people come home

--- homeaway.py
+++ homeaway.py
@@ -17,32 +17,52 @@ class homeaway(appapi.AppDaemon):
 
   def initialize(self):
     # self.LOGLEVEL="DEBUG"
-    self.log("homeaway App")
+    try:
+      self.log("homeaway App")
+    except IndexError:
+      self.restart_app()
+      self.exit()
+      
     # setup room structure
     # {"room":{["list of device trackers associated with this room. Get names from Home Assistant"],"lights":{"device name":"on/off",etc:etc}}}
     #
+    self.home_location=[]
+    self.home_location=eval(self.args["homelocation"])
+    self.log("homelocation={}".format(self.home_location))
     self.rooms={"master":{"occupants":["device_tracker.scox1209_scox1209","device_tracker.turboc1208_cc1208"],
-                          "lights":{"light.master_light_switch":"off", "light.master_floor_light":"off",
+                          "away":{"lights":{"light.master_light_switch":"off", "light.master_floor_light":"off",
                                     "switch.master_fan":"off", "switch.master_toilet_fan":"off",
                                     "switch.master_toilet_light":"off"}},
+                          "home":{"lights":None}},
                 "sam":{"occupants":["device_tracker.scox0129_sc0129"],
-                       "lights":{"light.sam_fan_switch":"off","light.sam_light_switch":"off",
+                       "away":{"lights":{"light.sam_fan_switch":"off","light.sam_light_switch":"off",
                                  "switch.sam_toilet_light":"off","switch.sam_toilet_fan":"off","switch.sam_vanity_switch":"off"}},
+                       "home":{"lights":None}},
                 "wholehouse":{"occupants":["device_tracker.scox1209_scox1209","device_tracker.turboc1208_cc1208","device_tracker.scox0129_sc0129","device_tracker.ccox0605_ccox0605"],
-                              "lights":{"group.whole_house":"off"}},
+                              "away":{"lights":{"group.whole_house":"off"}},
+                              "home":{"lights":None}},
                 "charlie":{"occupants":{"device_tracker.ccox0605_ccox0605"},
-                           "lights":{"light.charile_light_switch":"off","light.charlie_fan_switch":"50"}}}
+                           "away":{"lights":{"light.charile_light_switch":"off","light.charlie_fan_switch":"125"}},
+                           "home":{"lights":{"light.charlie_fan_switch":"255"}}}}
     
     self.tzoffset=-6
     # Setup Callback
     #self.run_minutely(self.timer_handler,datetime.datetime.now())          # for debugging
     self.run_every(self.timer_handler,datetime.now()+timedelta(minutes=15),15*60)
+    self.listen_state(self.homeaway_state_changed,"device_tracker")
 
-    self.checkHomeState()  # Lets see what our condition is now rather than waiting 15 minutes.
+    self.checkHomeState("initialize")  # Lets see what our condition is now rather than waiting 15 minutes.
 
   # Timer handle just calls checkHomeState so check home state can be called from other places as well
   def timer_handler(self,kwargs):
-    self.checkHomeState()
+    self.checkHomeState("timer")                                                   # check the house state by default every 15 minutes
+
+  def homeaway_state_changed(self,entity,attribute,old,new,kwargs):
+    if not new==old:                                                         # it wasn't a non movement state change we actually moved
+      if (new in self.home_location) or (old in self.home_location):            # either new or old were at the home/house location
+        if not((new in self.home_location) and (old in self.home_location)):    # both new and old were not at the home/house  we didn't just switch between home and house
+          self.log("{} just either got home or left home".format(entity))
+          self.checkHomeState("state")                                             # someone actually came home or left home so check the house state
 
   # most of the work is done here
   def _checklocationstate(self,location,occupants):
@@ -61,19 +81,19 @@ class homeaway(appapi.AppDaemon):
     return (anyonehome,noonehome,everyonehome) 
 
   def anyone_home(self):
-    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    anyonehome,noonehome,everyonehome=self._checklocationstate(self.home_location,self.rooms["wholehouse"]["occupants"])
     return anyonehome
   
   def everyone_home(self):
-    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    anyonehome,noonehome,everyonehome=self._checklocationstate(self.home_location,self.rooms["wholehouse"]["occupants"])
     return everyonehome
 
   def noone_home(self):
-    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+    anyonehome,noonehome,everyonehome=self._checklocationstate(self.home_location,self.rooms["wholehouse"]["occupants"])
     return noonehome    
   
-  def checkHomeState(self):
-    anyonehome,noonehome,everyonehome=self._checklocationstate(["home","house"],self.rooms["wholehouse"]["occupants"])
+  def checkHomeState(self,source):
+    anyonehome,noonehome,everyonehome=self._checklocationstate(self.home_location,self.rooms["wholehouse"]["occupants"])
 
     self.log("everyone_home={} anyone_home={} noone_home={}".format(self.everyone_home(),self.anyone_home(),self.noone_home()))
     #self.log("rooms={}".format(self.rooms))
@@ -90,7 +110,7 @@ class homeaway(appapi.AppDaemon):
       for room in self.rooms:
         if not self.room_occupants_home(self.rooms[room]["occupants"]):  # if the room occupants are not home
           self.log("room {} occupants {} are not home".format(room,self.rooms[room]["occupants"]))
-          light_list=self.build_light_list(self.rooms[room]["lights"])
+          light_list=self.build_light_list(self.rooms[room]["away"]["lights"])
           for light in light_list:                       # loop through the rooms lights
             # what is the last time the lights state was changed
             last_update=datetime.strptime(self.get_state(light,"all")["last_updated"],"%Y-%m-%dT%H:%M:%S.%f+00:00")+timedelta(hours=self.tzoffset)
@@ -115,6 +135,26 @@ class homeaway(appapi.AppDaemon):
               self.log("light {} was last turned on at {} which is less than 15 minutes ago so leave it on".format(light,last_update))
         else:
           self.log("room {} occupants {} are home".format(room,self.rooms[room]["occupants"]))
+          if state=="home":                               # deal with any ligths to turn on as a result of someone coming home. ignore if due to timer
+            light_list=self.build_light_list(self.rooms[room]["home"]["lights"])
+            for light in light_list:                       # loop through the rooms lights
+              # what is the last time the lights state was changed
+              if light_list[light]=="off":                 # if the light should be off
+                if self.get_state(light)=="on":
+                  self.log("turning off {}".format(light))
+                  self.turn_off(light)                                       # turn off the light
+              elif light_list[light]=="on":
+                if self.get_state(light)=="off":
+                  self.log("turning on {}".format(light))                    # if the light should be on
+                  self.turn_on(light)                                        # turn on the light
+              else:                                                          # it wasn't an on or off command so it should be a power setting for a dimmer.
+                try:
+                  dim=int(light_list[light])
+                except ValueError:
+                  self.log("Transfering to unknown light state {}".format(light))
+                if self.get_state(light)=="on":                              # only adjust the power setting if the light is already on.
+                  self.log("adjusting power to {}".format(dim))
+                  self.turn_on(light,brightness=dim)
     else:
       self.log("everyone is home")
   
@@ -151,7 +191,7 @@ class homeaway(appapi.AppDaemon):
     super(homeaway,self).log("{} - ({}) {}".format(func,str(line),msg),level)
 
   # set the house state based on input_select in HA
-def set_house_state(self,entity,state):
+  def set_house_state(self,entity,state):
     if self.entity_exists(entity):
       self.select_option(entity,state)
       retval=self.get_state(entity)
@@ -160,7 +200,7 @@ def set_house_state(self,entity,state):
     return(retval)
 
   # get get house state based on input_select in HA
-def get_house_state(self,entity):
+  def get_house_state(self,entity):
     if self.entity_exists(entity):
       state=self.get_state(entity)
       self.log("house state={}".format(state),"DEBUG")
@@ -168,4 +208,19 @@ def get_house_state(self,entity):
       state=None
     return(state)
 
+  def restart_app(self):
+    import os
+    import fnmatch
+    import subprocess
+
+    matches=[]
+    module=self.args["module"]
+    module=module+".py"
+    path=self.config["AppDaemon"]["app_dir"]
+    for root,dirnames,filenames in os.walk(path):
+      #self.log("root={} dirnames={} filenames={}".format(root,dirnames,filenames))
+      for filename in fnmatch.filter(filenames, module):
+        matches.append(os.path.join(root,filename))
+    self.log("restarting {}".format(matches))
+    subprocess.call(["touch",matches[0]])
 
